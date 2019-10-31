bccbf5a2741ff06f2df4db0107beefb7b9545ffd
ClaireDL <cl.delleluche@gmail.com>
2019-10-31 17:38:00 +0000

    checked state of program

--- src/main/scala/TransactionAnalyser.scala
+++ src/main/scala/TransactionAnalyser.scala
@@ -1,8 +1,5 @@
 package com.clairedl.scala
 
-import scala.math.BigDecimal.RoundingMode
-
-<<<<<<< HEAD
 class DataAnalyser(loader: TransactionLoader, filter: TransactionsFilter) {
 
   val transactions = loader.load()
@@ -42,63 +39,4 @@ class DataAnalyser(loader: TransactionLoader, filter: TransactionsFilter) {
       }
       .toMap
   }
-=======
-abstract class AnalysisPrep(loader: TransactionLoader, filter: TransactionsFilter) {
-  val transactions = loader.load()
-  val filtered = filter.filter(transactions)
-}
-
-class SumByAccountId(loader: TransactionLoader, filter: TransactionsFilter)
-  extends AnalysisPrep(loader, filter) {
-
-    def analyse(): Map[String, Double] = {
-      filtered
-        .groupBy(_.accountId) // Same as .groupBy(tr => tr.accountId)
-        .mapValues { trs =>
-          BigDecimal(
-            trs.map(tr =>
-              tr.amount)
-              .sum)
-            .setScale(2, RoundingMode.HALF_EVEN)
-            .toDouble
-        }
-        .toMap
-    }
-}
-
-class AverageByAccountId(loader: TransactionLoader, filter: TransactionsFilter)
-  extends AnalysisPrep(loader, filter) {
-
-    def analyse(): Map[String, Double] = {
-      filtered
-        .groupBy(_.accountId)
-        .mapValues { trs =>
-          BigDecimal(
-            trs.map(tr =>
-              tr.amount / trs.size)
-              .sum)
-            .setScale(2, RoundingMode.HALF_EVEN)
-            .toDouble
-        }
-        .toMap
-    }
-}
-
-class AverageByAccountIdByDay(loader: TransactionLoader, filter: TransactionsFilter)
-  extends AnalysisPrep(loader, filter) {
-
-    def analyse(): Map[(String, Int), Double] = {
-      filtered
-        .groupBy(tr => (tr.accountId, tr.day))
-        .mapValues { trs =>
-          BigDecimal(
-            trs.map(tr =>
-              tr.amount / trs.size)
-              .sum)
-            .setScale(2, RoundingMode.HALF_EVEN)
-            .toDouble
-        }
-        .toMap
-    }
->>>>>>> f618dbe2ef1665489b61ded5895cbe556f39266f
 }
