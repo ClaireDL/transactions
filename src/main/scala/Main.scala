8b58679c8f166e19ec5bbdb233571c719c3bf92a
ClaireDL <cl.delleluche@gmail.com>
2019-11-03 18:00:33 +0000

    added logging

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,19 @@
 package com.clairedl.scala
 
+import org.slf4j.LoggerFactory
+
 object Main extends App {
   val loader = new FileTransactionLoader("transactions.txt")
-//  val filter = new LowTransactionsFilter(400)
+  //  val filter = new LowTransactionsFilter(400)
   val filter = new NoFilter()
 
   val data = new DataAnalyser(loader, filter)
+  val logger = LoggerFactory.getLogger(getClass.getSimpleName)
+  logger.info("New object created: {}", data)
+
   val averageByAccountId = data.averageByAccountId()
   println(averageByAccountId.mkString("\n"))
-//  val averageByAccountIdByDay = data.averageByAccountIdByDay()
+
+  val averageByAccountIdByDay = data.averageByAccountIdByDay()
 //  println(averageByAccountIdByDay.mkString("\n"))
+}
--- src/main/scala/TransactionAnalyser.scala
+++ src/main/scala/TransactionAnalyser.scala
@@ -1,41 +1,48 @@
 package com.clairedl.scala
 
+import org.slf4j.LoggerFactory
+
 class DataAnalyser(loader: TransactionLoader, filter: TransactionsFilter) {
 
   val transactions = loader.load()
   val filtered = filter.filter(transactions)
 
+  val logger = LoggerFactory.getLogger(getClass.getSimpleName)
+  logger.trace("Transactions loaded and filtered")
+
   def groupedByAccountId: Map[String, List[Transaction]] =
     filtered.groupBy(_.accountId) // Same as .groupBy(tr => tr.accountId)
 
   def sumByAccountId(): Map[String, Double] = {
+    logger.info("Starting analysis")
     groupedByAccountId
       .mapValues { trs =>
-        trs.map(tr =>
+        Math.round((trs.map(tr =>
           tr.amount)
-          .sum
+          .sum) * 100.00) / 100.00
       }
       .toMap
   }
 
   def averageByAccountId(): Map[String, Double] = {
+    logger.info("Starting analysis")
     groupedByAccountId
       .mapValues { trs =>
-        trs.map(tr =>
-          Math.round((tr.amount / trs.size)) * 100 / 100)
-          .sum
-          .toDouble
+        Math.round((trs.map(tr =>
+          tr.amount / trs.size)
+          .sum) * 100.00) / 100.00
       }
       .toMap
   }
 
   def averageByAccountIdByDay(): Map[(String, Int), Double] = {
+    logger.info("Starting analysis")
     filtered
       .groupBy(tr => (tr.accountId, tr.day))
       .mapValues { trs =>
-        trs.map(tr =>
+        Math.round((trs.map(tr =>
           tr.amount / trs.size)
-          .sum
+          .sum) * 100.00) / 100.00
       }
       .toMap
   }
