9c0e88970767c33697eff5a4763072abc9d240cc
ClaireDL <cl.delleluche@gmail.com>
2019-11-19 11:49:42 +0000

    FilterAdapter as abstract class

--- src/main/scala/com/clairedl/scala/FilterAdapter.scala
+++ src/main/scala/com/clairedl/scala/FilterAdapter.scala
@@ -2,7 +2,9 @@ package com.clairedl.scala
 
 import com.colofabrix.scala.designpatterns.filters._
 
-class NoopFilterAdapter() extends TransactionsFilter {
+abstract class FilterAdapter extends TransactionsFilter
+
+class NoopFilterAdapter() extends FilterAdapter {
   def filter(transactions: List[Transaction]): List[Transaction] = {
     GenericTransactionListFilter
       .noopFilter(transactions)
@@ -10,15 +12,15 @@ class NoopFilterAdapter() extends TransactionsFilter {
   }
 }
 
-class DayFilterAdapter(day: Int) extends TransactionsFilter {
+class DayFilterAdapter(day: Int) extends FilterAdapter {
   def filter(transactions: List[Transaction]): List[Transaction] = {
-  GenericTransactionListFilter
-  .dayFilter(transactions, day)
-  .getFilteredList()
+    GenericTransactionListFilter
+      .dayFilter(transactions, day)
+      .getFilteredList()
   }
 }
 
-class AmountRangeFilter(min: Double, max: Double) extends TransactionsFilter {
+class AmountRangeFilter(min: Double, max: Double) extends FilterAdapter {
   def filter(transactions: List[Transaction]): List[Transaction] = {
     GenericTransactionListFilter
       .amountRangeFilter(transactions, min, max)
